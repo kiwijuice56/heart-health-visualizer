12be2ec88eacdc029a1d02e5b3028d547489f889
Eric Alfaro <ealfaro@mit.edu>
2025-02-27 08:31:37 -0500

    Basic camera testing

--- godot-project/addons/GodotAndroidCamera/export_plugin.gd
+++ godot-project/addons/GodotAndroidCamera/export_plugin.gd
@@ -31,11 +31,7 @@ class AndroidExportPlugin extends EditorExportPlugin:
 			return PackedStringArray([_plugin_name + "/bin/release/" + _plugin_name + "-release.aar"])
 
 	func _get_android_dependencies(platform, debug):
-		# TODO: Add remote dependices here.
-		if debug:
-			return PackedStringArray([])
-		else:
-			return PackedStringArray([])
+		return PackedStringArray(["androidx.core:core:1.9.0"])
 
 	func _get_name():
 		return _plugin_name
--- godot-project/main/main.gd
+++ godot-project/main/main.gd
@@ -5,12 +5,34 @@ class_name Main extends Node
 
 var fractal_parameters: PackedFloat64Array
 
+
+var _plugin_name: String = "GodotAndroidCamera"
+var _plugin_singleton
+
 func _ready() -> void:
+	# Initialize camera plugin
+	if Engine.has_singleton(_plugin_name):
+		_plugin_singleton = Engine.get_singleton(_plugin_name)
+	else:
+		printerr("Initialization error: unable to access the java logic")
+	
+	print(_plugin_singleton.requestCameraPermissions())
+	
+	# Initialize parameters
+	
 	fractal_parameters.resize(3)
 	fractal_parameters.fill(0)
 	
+	# Connect debug UI components
+	
+	%RecordButton.pressed.connect(_on_record_pressed)
+	
 	for i in range(1, 4):
 		get_node("%%P%dSlider" % i).value_changed.connect(_on_p_changed.bind(i))
+
+func _on_record_pressed() -> void:
+	print(_plugin_singleton.isCameraAvailable())
+	_plugin_singleton.openCamera()
 
 func _on_p_changed(val: float, idx: int) -> void:
 	fractal_parameters[idx - 1] = val
