5c468145ea6e0d780935d796593be4f31ea5cf0a
Eric Alfaro <ealfaro@mit.edu>
2025-03-02 17:13:29 -0500

    Readds working plugin

---
+++ godot-project/addons/GodotAndroidCamera/export_plugin.gd
@@ -0,0 +1,32 @@
+@tool
+extends EditorPlugin
+
+const _plugin_name: String = "GodotAndroidCamera"
+
+var export_plugin: AndroidExportPlugin
+
+func _enter_tree() -> void:
+	export_plugin = AndroidExportPlugin.new()
+	add_export_plugin(export_plugin)
+
+func _exit_tree() -> void:
+	remove_export_plugin(export_plugin)
+	export_plugin = null
+
+class AndroidExportPlugin extends EditorExportPlugin:
+	func _supports_platform(platform):
+		if platform is EditorExportPlatformAndroid:
+			return true
+		return false
+
+	func _get_android_libraries(platform, debug):
+		if debug:
+			return PackedStringArray([_plugin_name + "/bin/debug/" + _plugin_name + "-debug.aar"])
+		else:
+			return PackedStringArray([_plugin_name + "/bin/release/" + _plugin_name + "-release.aar"])
+
+	func _get_android_dependencies(platform, debug):
+		return PackedStringArray(["androidx.core:core:1.9.0"])
+
+	func _get_name():
+		return _plugin_name
--- godot-project/main/main.gd
+++ godot-project/main/main.gd
@@ -3,24 +3,36 @@ class_name Main extends Node
 @onready var fractal_flame_renderer: FractalFlameRenderer = %FractalFlameRenderer
 @onready var render_canvas: TextureRect = %RenderCanvas
 
+var camera: AndroidCamera
 var fractal_parameters: PackedFloat64Array
-
+var recording: bool = false
 
 func _ready() -> void:
 	# Initialize parameters
-	
 	fractal_parameters.resize(3)
 	fractal_parameters.fill(0)
 	
+	# Initialize camera
+	camera = AndroidCamera.new()
+	camera.camera_frame.connect(_on_camera_frame)
+	
 	# Connect debug UI components
-	
 	%RecordButton.pressed.connect(_on_record_pressed)
-	
 	for i in range(1, 4):
 		get_node("%%P%dSlider" % i).value_changed.connect(_on_p_changed.bind(i))
 
+func _on_camera_frame(frame: ImageTexture) -> void:
+	%CameraCanvas.texture = frame
+
 func _on_record_pressed() -> void:
-	pass
+	if not camera.request_camera_permissions():
+		printerr("Camera permissions denied.")
+		return
+	recording = not recording
+	if recording:
+		camera.start_camera(256, 256, 60, false)
+	else:
+		camera.stop_camera()
 
 func _on_p_changed(val: float, idx: int) -> void:
 	fractal_parameters[idx - 1] = val
