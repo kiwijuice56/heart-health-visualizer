cd2cf78d3244db08eb8f46772ffe81c415b31d83
Eric Alfaro <ealfaro@mit.edu>
2025-03-03 13:19:43 -0500

    Improved recording parameters

--- godot-project/main/main.gd
+++ godot-project/main/main.gd
@@ -5,6 +5,7 @@ class_name Main extends Node
 
 @onready var ppg_chart: Chart = %PpgChart
 @onready var record_button: Button = %RecordButton
+@onready var render_button: Button = %RenderButton
 
 var fractal_flame_renderer: FractalFlameRenderer 
 var ppg_reader: PpgReader
@@ -12,8 +13,11 @@ var camera: AndroidCamera
 
 var fractal_parameters: PackedFloat64Array
 var recording: bool = false
+var rendering: bool = false
 
 func _ready() -> void:
+	set_process(rendering)
+	
 	# Initialize GDextension modules
 	fractal_flame_renderer = FractalFlameRenderer.new()
 	ppg_reader = PpgReader.new()
@@ -28,12 +32,13 @@ func _ready() -> void:
 	
 	# Connect debug UI components
 	record_button.pressed.connect(_on_record_pressed)
+	render_button.pressed.connect(_on_render_pressed)
 	for i in range(1, 4):
 		get_node("%%P%dSlider" % i).value_changed.connect(_on_p_changed.bind(i))
 
 func _on_camera_frame(frame: ImageTexture) -> void:
 	camera_canvas.texture = frame
-	var ppg_val: int = ppg_reader.read_ppg_from_image(frame.get_image(), Rect2i(Vector2i(128, 128), Vector2i(768, 768)))
+	var ppg_val: int = ppg_reader.read_ppg_from_image(frame.get_image(), Rect2i(Vector2i(0, 0), Vector2i(1080, 1080)))
 	ppg_chart.add_point(ppg_val, Time.get_unix_time_from_system())
 
 func _on_record_pressed() -> void:
@@ -42,9 +47,13 @@ func _on_record_pressed() -> void:
 		return
 	recording = not recording
 	if recording:
-		camera.start_camera(1024, 1024, 30, true)
+		camera.start_camera(1080, 1080, 60, false)
 	else:
 		camera.stop_camera()
+
+func _on_render_pressed() -> void:
+	rendering = not rendering
+	set_process(rendering)
 
 func _on_p_changed(val: float, idx: int) -> void:
 	fractal_parameters[idx - 1] = val
