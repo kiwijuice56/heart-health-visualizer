ea49afd74de648097b1aff48bcc6112343b34af6
Eric Alfaro <ealfaro@mit.edu>
2025-06-11 07:38:32 -0500

    About page

--- godot-project/main/main.gd
+++ godot-project/main/main.gd
@@ -2,15 +2,22 @@ class_name Main extends Node
 
 @onready var record_button: Button = %CreateRecordingButton
 @onready var clear_button: Button = %ClearDataButton
+@onready var about_button: Button = %AboutButton
 
 @onready var recording_menu: RecordingMenu = %RecordingMenu
 @onready var recording_progress_menu: Menu = %RecordingProgressMenu
+@onready var about_menu: Menu = %AboutMenu
 
 func _ready() -> void:
 	# Connect UI components
 	record_button.pressed.connect(_on_record_pressed)
 	clear_button.pressed.connect(_on_clear_pressed)
+	about_button.pressed.connect(_on_about_pressed)
+	
 	recording_menu.recording_requested.connect(_on_recording_requested)
+
+func _on_about_pressed() -> void:
+	about_menu.enter()
 
 func _on_recording_requested() -> void:
 	if not Ref.recorder.recording:
