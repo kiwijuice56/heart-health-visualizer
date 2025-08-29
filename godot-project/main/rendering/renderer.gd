2063b5037950ad151d9959ac4337cb372df5504c
Eric Alfaro <ealfaro@mit.edu>
2025-08-28 22:12:28 -0500

    Minor edits to fractal

--- godot-project/main/rendering/renderer.gd
+++ godot-project/main/rendering/renderer.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 	fractal_flame = FractalFlameRenderer.new()
 
 func render_recording(recording: Recording) -> ImageTexture:
-	fractal_flame.fractal_parameters = PackedFloat64Array([
-		recording.overall_score_fourier, 
-		recording.overall_score_linear_slope + recording.overall_score_rising_edge_area, 
-		recording.overall_score_peak_detection
+	fractal_flame.fractal_parameters = PackedFloat64Array([ 
+		(recording.overall_score_linear_slope + recording.overall_score_rising_edge_area) / 2.0, 
+		recording.overall_score_peak_detection,
+		recording.overall_score_fourier
 	])
 	return fractal_flame.render(image_size, 200, 4096, 512, 32)
 
