9e3e43886851210b03c3b920b9f7b43de1d743ff
Eric Alfaro <ealfaro@mit.edu>
2025-12-08 20:45:03 -0500

    User ID on thumbnail

--- godot-project/main/ui/menu/history/saved_recording_panel/saved_recording_panel.gd
+++ godot-project/main/ui/menu/history/saved_recording_panel/saved_recording_panel.gd
@@ -12,6 +12,8 @@ func _on_tap_pressed() -> void:
 
 func initialize(new_recording: Recording) -> void:
 	recording = new_recording
+	%NameLabel.text = recording.user_id
+	%NameLabel.visible = recording.user_id.length() > 0
 	%DateLabel.text = "%s/%s/%s" % [recording.time.year, recording.time.month, recording.time.day]
 	%ScoreLabel.text = str(int(100 * recording.health_score))
 	%Render.texture = recording.render
